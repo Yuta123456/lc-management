0722bb46b877f4d98edc5c1a19fe295b32e17752
Yuta123456 <yuuta09090530@icloud.com>
2023-02-03 21:32:45 +0900

    mainpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@
 
 import { userState } from "@/state/user";
 import { login } from "@/utils/login";
+import { Container, Heading } from "@chakra-ui/react";
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <Container maxW={"90vw"} margin="auto" padding="3.5rem 0">
+      <Heading>管理画面</Heading>
+    </Container>
+  );
 }
